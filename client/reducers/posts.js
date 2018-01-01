853cc1c484d3e4ed9ffa2e42cc47d3b4f9df3959
khalilliu <lkhaliliu@gmail.com>
2018-01-01 20:04:00 +0800

    learn-redux

---
+++ client/reducers/posts.js
@@ -0,0 +1,19 @@
+//a reducer take in two things
+// 1. the action(info about what happen)
+//2. copy of current data
+
+function posts(state=[],action){
+	switch(action.type){
+		case 'INCREMENT_LIKES':
+			const i = action.index;
+			return [... state.slice(0,i), 
+				{...state[i], likes: state[i].likes + 1},
+				...state.slice(i+1)
+			];
+		default:
+			return state;
+	}
+	return state;
+}
+
+export default posts;
---
+++ client/store.js
@@ -0,0 +1,34 @@
+import {createStore, compose} from 'redux';
+import {syncHistoryWithStore} from 'react-router-redux';
+import {browserHistory} from 'react-router';
+
+//import the root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+//create an object for the default data
+const defaultState = {
+	posts,
+	comments
+}
+
+//redux devtools
+const enhancers = compose(
+	window.devToolsExtension ? window.devToolsExtension() : f=>f
+)
+
+//react-router-redux
+const store = createStore(rootReducer,defaultState,enhancers);
+export const history = syncHistoryWithStore(browserHistory, store);
+
+//reducer hot reload
+if(module.hot){
+	module.hot.accept('./reducers/',()=>{
+		const nextRootReducer = require('./reducers/index').default;
+		store.replaceReducer(nextRootReducer);
+	})
+}
+
+export default store;
